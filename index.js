9575a7c8dc6ba242937e3cb913af2365204e3017
Veronicah <veronicah.gicheru@student.moringaschool.com>
2022-06-06 11:30:14 -0700

    commit tree 0c159e12f00d16f7afd0b3b7b2039369adf7ad8a

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution in this file!
+const companyName= 'Scuber'
+ mostProfitableNeighborhood='Chelsea'
+ let mostProfitableNeighborhood='Chelsea'
+ companyCeo='Susan Smith'
+ let companyCeo='Susan Smith'
+ 
+
